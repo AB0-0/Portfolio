7a447eea65d1dc4a8760407b5b6cc9100a3172a2
AB0-0 <ashleejennifer.lee0@gmail.com>
2025-11-12 22:56:49 +0800

    fixes

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ window.addEventListener('scroll', ()=>{
     const navbar=document.querySelector('.navbar');
     window.scrollY > 50 ? navbar.style.backgroundColor='#bbd9eb50' : navbar.style.backgroundColor='transparent';
 });
+
+//menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
+
+ 
+const navItems = navLinks.querySelectorAll('a');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('active');
+    });
+});
